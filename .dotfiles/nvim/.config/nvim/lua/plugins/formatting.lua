b7a469f80df9d5528f0bac64e562a259ced4a445
August Ek <august.ek90@gmail.com>
2026-03-14 10:28:42 +0100

    fix: do not try to format zsh with shfmt

--- .dotfiles/nvim/.config/nvim/lua/plugins/formatting.lua
+++ .dotfiles/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -6,6 +6,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         sh = { "shfmt" },
+        bash = { "shfmt" },
       },
       format_on_save = {
         lsp_fallback = true,
